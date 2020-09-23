672f589b8223df009569842944b439aaf8360244
Andre Gomez <andregc1983@gmail.com>
2020-09-22 22:01:22 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,28 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); // Instancia do caminho da view
+			Parent parent = loader.load(); // Carrega a view
+			Scene mainScene = new Scene(parent); // Cria a cena - Objeto da cena principal e passando o argumento "parent"
+			primaryStage.setScene(mainScene); // Seta como a cena principal
+			primaryStage.setTitle("Sample JavaFX application"); // Título da cena principal - é o nome que fica na parte superior da janela da aplicação.
+			primaryStage.show(); // Mostra a cena principal
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
