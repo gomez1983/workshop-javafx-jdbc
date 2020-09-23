87e7d08be54384e32a524911787bb233f6377524
Andre Gomez <andregc1983@gmail.com>
2020-09-23 20:47:10 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +13,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); // Instancia do caminho da view
-			Parent parent = loader.load(); // Carrega a view
-			Scene mainScene = new Scene(parent); // Cria a cena - Objeto da cena principal e passando o argumento "parent"
+			ScrollPane scrollPane = loader.load(); // Carrega a view
+			
+			scrollPane.setFitToHeight(true); // Faz a barra de menu ficar na altura da tela
+			scrollPane.setFitToWidth(true); // Faz a barra de menu ficar na largura da tela
+			
+			Scene mainScene = new Scene(scrollPane); // Cria a cena - Objeto da cena principal e passando o argumento "parent"
 			primaryStage.setScene(mainScene); // Seta como a cena principal
 			primaryStage.setTitle("Sample JavaFX application"); // Título da cena principal - é o nome que fica na parte superior da janela da aplicação.
 			primaryStage.show(); // Mostra a cena principal
