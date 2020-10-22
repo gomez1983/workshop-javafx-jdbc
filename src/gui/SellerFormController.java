d8bbd5d40cbda14b62f4051aae7bc11d10ffc437
Andre Gomez <andregc1983@gmail.com>
2020-10-21 21:11:52 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,145 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+//--- Dependência para o departamento	
+	
+	private Seller entity;
+	
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>(); // Permite que outros objetos se inscrevam nessa lista para receber os eventos.
+	
+//------Declaração dos componentes da tela
+
+	@FXML
+	private TextField txtId; // Atributos para a caixa de preenchimento de Id
+
+	@FXML
+	private TextField txtName; // Atributos para a caixa de preenchimento de Name
+
+	@FXML
+	private Label labelErrorName; // Atributos para o label de erros. Mensagem caso dê algo errado ao preencher o nome
+	
+	@FXML
+	private Button btSave; // Controllers dos botões "Save" e "Cancel"
+	
+	@FXML
+	private Button btCancel; // Controllers dos botões "Save" e "Cancel"
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+		
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) { // Controlador do botão "save"
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData(); // Responsável por pegar os dados das caixas dos formúlarios e instanciar o departamento
+			service.saveOrUpdate(entity); // Salva no banco de dados
+			notifyDataChangeListeners();// Faz a notificação
+			Utils.currentStage(event).close(); // Fecha a janela
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void notifyDataChangeListeners() { // Executa o método "onDataChanged" da interface "DataChangeListener"
+		for (DataChangeListener listener : dataChangeListeners) { // Um for para cada DataChangeListener listener pertencente a lista dataChangeListeners...
+			listener.onDataChanged(); // ... faz este método para o evento
+		}
+		
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error"); // Instanciou a sessão
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText())); // Pega o que for digitado na caixa do formulário
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) { // Se o nome for "nulo" ou for igual a espaços nulos (string vazio), a exceção é ativada.
+			exception.addError("name", "Field can't be empty");
+		}
+		obj.setName(txtName.getText()); // Pega o nome digitado
+		
+		if (exception.getErrors().size() > 0) { // Testa se a coleção de erros tem ao menos 1 erro. Se for verdade, lança a exceção.
+			throw exception;
+		}
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) { // Controlador do botão "cancel"
+		Utils.currentStage(event).close(); // Fecha a janela
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30); // Define a quantidade máxima de caracteres
+	}
+		
+	public void updateFormData() { // Dados que estiverem dentro do objeto entity
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		txtId.setText(String.valueOf(entity.getId())); // Tem que converter o valor do Id (Inteiro) para String.
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) { // Percorre a coleção, carrega os erros e preenche os erros nas caixas de texto do programa.
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) { // Se existir a chave "name"...
+			labelErrorName.setText(errors.get("name")); // Pega a mensagem correspondente ao campo name e seta ela em "labelErrorName".
+		}
+	}
+}
+
+
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -113,29 +118,29 @@ public class SellerListController implements Initializable, DataChangeListener {
 	}
 	
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) { // Quando criamos uma janela de diálogo, temos que informar pra ela quem que é o stage que criou essa janela
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load(); // Carrega o painel
-//			
-//			
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj); // Injeta nesse controlador o departamento
-//			controller.setSellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this); // Se inscreve para receber o evento
-//			controller.updateFormData(); // Método para carregar os dados desse objeto no formulário		
-//			
-//			Stage dialogStage = new Stage();
-//			dialogStage.setTitle("Enter Seller data");
-//			dialogStage.setScene(new Scene(pane)); // Elemento raiz da cena é "pane"
-//			dialogStage.setResizable(false); // Janela não poderá ser redimensionada
-//			dialogStage.initOwner(parentStage); // Função que define quem é o stage pai dessa janela
-//			dialogStage.initModality(Modality.WINDOW_MODAL); // Ela é modal. Ficará travada
-//			dialogStage.showAndWait();
-//			
-//		}
-//		catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load(); // Carrega o painel
+			
+			
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj); // Injeta nesse controlador o departamento
+			controller.setSellerService(new SellerService());
+			controller.subscribeDataChangeListener(this); // Se inscreve para receber o evento
+			controller.updateFormData(); // Método para carregar os dados desse objeto no formulário		
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Seller data");
+			dialogStage.setScene(new Scene(pane)); // Elemento raiz da cena é "pane"
+			dialogStage.setResizable(false); // Janela não poderá ser redimensionada
+			dialogStage.initOwner(parentStage); // Função que define quem é o stage pai dessa janela
+			dialogStage.initModality(Modality.WINDOW_MODAL); // Ela é modal. Ficará travada
+			dialogStage.showAndWait();
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
